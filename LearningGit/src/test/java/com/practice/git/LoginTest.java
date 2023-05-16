5fe705ffff943ecd916a1a7ff090678b1345b294
RAHWA <RAHWA@10.0.0.100>
2023-05-16 15:13:01 -0400

    added new main method

--- LearningGit/src/test/java/com/practice/git/LoginTest.java
+++ LearningGit/src/test/java/com/practice/git/LoginTest.java
@@ -1,5 +1,10 @@
 package com.practice.git;
 
 public class LoginTest {
-
+  public static void main(String[] args) {
+	System.out.println("hello all, I am jonny. I glad i joined this team !");
+	
+    }
+	
 }
+
